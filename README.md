bb7619381f827c6081863393d08fc3359f757f2a
kevin <kevinbejarano2806@gmail.com>
2025-01-22 21:59:22 -0300

    feature: seccion 8

--- README.md
+++ README.md
@@ -22,6 +22,11 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Reconstruir la base de datos con la seed
+```
+http://localhost:3000/api/v2/seed
+```
+
 ## Stack usado
 * MongoDB
 * Nestjs
--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -4,6 +4,8 @@ import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 import { isValidObjectId, Model } from 'mongoose';
 import { Pokemon } from './entities/pokemon.entity';
 import { InjectModel } from '@nestjs/mongoose';
+import { PaginationDto } from '../common/dto/pagination.dto';
+import { skip } from 'node:test';
 
 @Injectable()
 export class PokemonService {
@@ -27,8 +29,18 @@ export class PokemonService {
     }
   }
 
-  findAll() {
-    return `This action returns all pokemon`;
+  async findAll(paginationDto: PaginationDto) {
+
+    const { limit = 10, offset = 0 } = paginationDto;
+
+    return this.pokemonModel.find()
+      .limit(limit)
+      .skip(offset)
+      .sort({
+        no: 1
+      })
+      .select('-__v');
+
   }
 
   //cambiar a termn
